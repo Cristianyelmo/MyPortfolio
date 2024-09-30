e95b88f1c9212661023d655525f41ed0f0aabc74
Cristianyelmo <cristianama2019@gmail.com>
2024-09-30 16:29:37 -0300

    test marco

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -20,10 +20,13 @@ function AboutMe() {
 <section className="flex md:justify-center flex-col md:flex-row items-center relative">
 <img src="/cloud2.webp" width={700} alt="" className="absolute z-10 right-0 md:top-0" />
 
-<div className="relative">
-<img src="/marco.webp" alt="" width={453} height={600}  className="absolute z-20 w-[453px] " />
+<div className="relative flex justify-center items-center">
+ <img src="/marco.webp" alt="" width={453} height={600}  className="absolute z-30 w-[453px] " /> 
+<img src="/rap4.webp" alt="" width={253} height={600}  className="absolute z-20 w-[300px] " />
 <img src="/marco1.webp" alt="" width={453} height={600} className=" z-10 w-[453px] "/>
 </div> 
+
+
 
 <div className="bg-black/40 glow-effect max-w-[600px]   z-20 h-full  p-3 m-3">
 <p className="text-white glow-text">Hola me llamo cristian y soy desarrollador full stack cuando tenia 8 años me culiee un perro en la calle</p>
--- src/hooks/useIsVisible.tsx
+++ src/hooks/useIsVisible.tsx
@@ -16,28 +16,35 @@ function useVisibilityTracker() {
     tecnologias: useRef(null),
     contacto: useRef(null),
   };
-  
+ 
   useEffect(() => {
+
+  
     const observer = new IntersectionObserver(
+      
       (entries) => {
         entries.forEach((entry) => {
           const id = entry.target.id;
-        
+    
           if (entry.isIntersecting) {
             setVisibility((prev) => ({ ...prev, [id]: true }));
            
           } else {
             setVisibility((prev) => ({ ...prev, [id]: false }));
           }
+        
         });
+
+        
       },
       { threshold: 0.5 }
+      
     );
 
 
+const holas = observer
 
-
-  
+  console.log(holas)
     Object.values(elementRefs).forEach((ref) => {
       if (ref.current) {
         observer.observe(ref.current);
