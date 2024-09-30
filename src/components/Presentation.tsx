e7cef3e9c3bf7b5fca71ee71a166444ab89b0955
Cristianyelmo <cristianama2019@gmail.com>
2024-09-30 11:14:32 -0300

    remove querubines

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -16,67 +16,29 @@ function Presentation() {
 
 
   return (
-    <div ref={elementRef}  className="h-screen pt-10">
+    <div ref={elementRef}  className="h-screen pt-10 content-center">
   
 
-<section className="text-white glow-text text-xl relative ">
+<section className="text-white glow-text text-xl ">
+  <div className='flex justify-center'>
+
+ 
+ 
+ 
     <div className="z-20 text-center flex flex-col items-center relative">
 <h2 className="text-sm md:text-base z-20">Hola soy cristian amarilla y soy</h2>
 <h1 className="md:text-7xl mt-7 text-3xl z-20">Desarrollador</h1>
 
-<img src="/photoroom.webp" alt="" className="absolute z-50 top-[100%]" />
+<img src="/photoroom.webp" alt="" className="" />
 </div>
 
-
-
-<img src="/cloud2.webp" alt="" width={600} className="absolute z-10 right-0 top-0" />
-<img src="/cloud1.webp" alt="" width={600} className="absolute z-10 left-0 top-0" />
-
-<div className="relative ml-10 top-[150px] md:top-0">
-  <img 
-    src="/Querubin1.webp" 
-    alt="" 
-    width={120} 
-    loading="lazy" 
-    height={120} 
-    className="z-30 absolute w-[100px] md:w-[120px] lazyload" 
-  />
-  <img 
-    src="/Querubin1-2.webp" 
-    alt="" 
-    width={120} 
-    loading="lazy" 
-    height={120} 
-    className="z-20 absolute  w-[100px] md:w-[120px] lazyload" 
-  />
 </div>
 
-<div className="relative flex justify-end mr-10 top-[150px] md:top-0">
-  <img 
-    src="/Querubin2.webp" 
-    alt="" 
-    loading="lazy" 
-    width={150} 
-    height={150} 
-    className="z-30 absolute w-[120px] md:w-[150px] lazyload" 
-  />
-  <img 
-    src="/Querubin2-1.webp" 
-    loading="lazy" 
-    alt="" 
-    width={150} 
-    height={150} 
-    className="z-20 absolute w-[120px] md:w-[150px] lazyload" 
-  />
-  <img 
-    src="/Querubin2-2.webp" 
-    alt="" 
-    loading="lazy" 
-    width={150} 
-    height={150} 
-    className="z-20 absolute  w-[120px] md:w-[150px] lazyload" 
-  />
-</div>
+<img src="/cloud2.webp" alt="" width={200} className="absolute z-10 right-0 top-0 md:w-[550px]" />
+<img src="/cloud1.webp" alt="" width={200} className="absolute z-10 left-0 top-0 md:w-[550px]" />
+
+
+
 
 </section>
 
