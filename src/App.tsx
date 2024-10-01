560165ab1d7033ceb19d73d47babf784a1515667
Cristianyelmo <cristianama2019@gmail.com>
2024-10-01 12:50:09 -0300

    test optimization

--- src/App.tsx
+++ src/App.tsx
@@ -56,52 +56,64 @@ document.body.style.backgroundSize = "auto";
     return () => {
       clearTimeout(timeoutId);
     };
-  },[])
+  },[]) 
 
+
+
+
+ 
+
+
+ 
   return (
-    <div className={``}>
-   <div className={`fixed ${classname} z-50 black-background inset-0 bg-black flex justify-center items-center text-white text-center`}>
-    cargando...
-</div>
-   <Modelsx />
-      
-      <div  className="flex index-button flex-col space-y-4 text-white p-3 fixed bottom-4 right-4">
-      
-      
 
-      {isVisible && (
-        <button className="p-4 bg-black/50 rounded-full"
-          onClick={scrollToTop}
-         
-        >
-          ↑
-        </button>
+<div >
+
+{classname !== 'hidden' && <div className={`fixed ${classname} z-50 black-background inset-0 bg-black flex justify-center items-center text-white text-center`}>
+ cargando...
+</div>}
+<Modelsx /> 
+   
+   <div  className="flex index-button flex-col space-y-4 text-white p-3 fixed bottom-4 right-4">
+   
+   
+
+   {isVisible && (
+     <button className="p-4 bg-black/50 rounded-full"
+       onClick={scrollToTop}
       
-      )}
+     >
+       ↑
+     </button>
+   
+   )}
 
 <button className="bg-black/90 text-white p-1"
-       
-       onClick={() => Playbutton()}
-     >
-       {!playText ? "play" : "stop"}
-     </button>
-        </div>
+    
+    onClick={() => Playbutton()}
+  >
+    {!playText ? "play" : "stop"}
+  </button>
+     </div>
 
 
 
 
-      {playText && (
-        <div className="breakP z-50 bg-black/60 text-white max-w-[900px] min-h-[100px] p-4 fixed  bottom-4  ">
-          {playText && <Typewriter text={textChibi} speed={50} />}
-        </div>
-      )}
+   {playText && (
+     <div className="breakP z-50 bg-black/60 text-white max-w-[900px] min-h-[100px] p-4 fixed  bottom-4  ">
+   <Typewriter text={textChibi} speed={50} />
+     </div>
+   )}
 
-      <Presentation />
-      <AboutMe />
-      <Technologies />
-      <Projects />
-      <Contact />
-    </div>
+   <Presentation />
+   <AboutMe />
+   <Technologies />
+   <Projects />
+   <Contact />
+   
+
+ </div>
+ 
   );
 }
 
