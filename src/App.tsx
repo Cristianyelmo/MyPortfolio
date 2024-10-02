95b399fd019bf322958e66a9c694d93181652a18
Cristianyelmo <cristianama2019@gmail.com>
2024-10-02 11:18:27 -0300

    viewport 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Presentation from "./components/Presentation"; 
-/* import AboutMe from "./components/AboutMe";  
- import Technologies from "./components/Technologies"; */
+/* import AboutMe from "./components/AboutMe";   */
+ import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact"; 
 import Modelsx from "./components/Chibi"; 
@@ -119,8 +119,8 @@ Playbutton,
       )} 
 
        <Presentation /> 
-  {/*    <AboutMe />  
-       <Technologies /> */}
+ {/*     <AboutMe />  */} 
+       <Technologies />
       <Projects />
       <Contact /> 
     </>
