705f0d8e12205f7f1948afa4d6348845e3e02dfa
Cristianyelmo <cristianama2019@gmail.com>
2024-10-02 10:51:52 -0300

    button customized

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,7 +3,34 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { MainHook } from "../Context/MainContext";
+function SamplePrevArrow(props:any) {
+  const {onClick } = props;
 
+ 
+  return (
+      
+    <div
+      className=" bg-black/70 rounded-full w-[50px] h-[50px] cursor-pointer glow-effect absolute text-2xl text-white flex justify-center items-center z-20 bottom-[50%] left-0"
+      onClick={onClick}
+    >
+
+&lt;
+    </div>
+  );
+}
+function SampleNextArrow(props:any) {
+  const {onClick } = props;
+  
+  return (
+      <div
+      className=" bg-black/70 rounded-full w-[50px] h-[50px] cursor-pointer glow-effect text-2xl absolute text-white flex justify-center items-center z-20 bottom-[50%] right-0"
+      onClick={onClick}
+    >
+
+&gt;
+    </div>
+  );
+}
 
 
 function Carousel() {
@@ -20,7 +47,10 @@ const widthResponsive = window.innerWidth
     infinite: true,
     speed: 500,
     slidesToShow: window.innerWidth < 600 ? 1 : 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    
+    nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />
   };
   return (
     <div className="slider-container z-20">
