507a54801baa1b42eacd1b6145355779d4ec12f4
Cristianyelmo <cristianama2019@gmail.com>
2024-10-04 16:06:27 -0300

    test mobile photo

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -16,15 +16,19 @@ function AboutMe() {
   }, [visibility.sobreMi, setTextchibi]);
 
   return (
-    <div ref={elementRefs.sobreMi} id="sobreMi"  className="md:min-h-screen" >
+    <div ref={elementRefs.sobreMi} id="sobreMi"  className="md:min-h-screen relative" >
       <h1 className="text-white glow-text text-center text-4xl">Sobre mi</h1>
-      <section className="flex md:justify-center flex-col md:flex-row items-center relative">
-        <img src="/cloud2.webp" width={700} alt="decorative" className="absolute z-10 right-0 md:top-0" />
-        <div className="relative flex justify-center items-center">
-          <img src="/marco.webp" alt="decorative" width={453} height={600} className="absolute z-30 w-[453px]" />
+      <img src="/cloud2.webp" width={700} alt="decorative" className="absolute z-10 right-0 md:top-0" />
+      <div  className="flex justify-center items-center md:h-screen" >
+      <section className="flex flex-col md:justify-center md:flex-row items-center ">
+      
+      {/*   <div className="relative flex justify-center items-center">
+           <img src="/marco.webp" alt="decorative" width={453} height={600} className="absolute z-30 w-[453px]" />
           <img src="/ssaaas.webp" alt="decorative" width={253} height={600} className="absolute z-20 w-[300px]" />
-          <img src="/marco1.webp" alt="decorative" width={453} height={600} className="z-10 w-[453px]" />
-        </div>
+          <img src="/marco1.webp" alt="decorative" width={453} height={600} className="z-10 w-[453px]" /> 
+          <img src="/xc (1).webp" alt="" width={300} height={300} className="z-20"/>
+        </div> */}
+        <img src="/xc (1).webp" alt="" width={300} height={300} className="z-20"/>
         <div className="bg-black/40 glow-effect max-w-[500px] z-20 h-full text-center flex justify-center items-center p-3 m-3">
           <p className="text-white glow-text font2">
             Hace más de un año que estoy en este camino de la programación, estudié en Digital House gracias a la beca de
@@ -35,6 +39,7 @@ function AboutMe() {
           </p>
         </div>
       </section>
+      </div>
     </div>
   );
 }
